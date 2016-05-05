61e062299b97b649f7b3b45134f0f5affe9b273d
Sam <ArcticEcho@users.noreply.github.com>
2016-05-05 14:06:04 +0100

    Fix end of word punctuation bug

--- UniStack/Extensions.cs
+++ UniStack/Extensions.cs
@@ -27,7 +27,7 @@ namespace UniStack
             " '", "' ", "'.", "',", "'?", "'!", "':", "';",
 
             // Misc.
-            ". ", ": ", "; ", "- ", "?", "!", "\\", "/", ",", " "
+            ". ", ": ", "; ", "- ", "?", "!", "\\", "/", ",", " ", "\r", "\n", "\t"
         };
         private static readonly char[] strEndPunc = new[]
         {
@@ -36,25 +36,32 @@ namespace UniStack
 
 
 
-
         // ~30% faster than using a compiled regex.
         public static Dictionary<string, uint> ToTermFrequencyDictionary(this string str)
         {
             var tfs = new Dictionary<string, uint>();
             var words = str.ToLowerInvariant().Split(wordDelimeters, StringSplitOptions.RemoveEmptyEntries);
-            var lastWordInd = words.Length - 1;
-            var lastWord = words[lastWordInd];
-
-            if (strEndPunc.Contains(lastWord[lastWord.Length]))
-            {
-                words[lastWordInd] = lastWord.Substring(0, lastWord.Length - 1);
-            }
 
             foreach (var wrd in words)
             {
                 var w = wrd;
 
                 if (w.All(c => !char.IsLetterOrDigit(c))) continue;
+
+                var charsToRem = 0;
+                for (var i = w.Length - 1; i > 0; i--)
+                {
+                    if (strEndPunc.Contains(w[i]))
+                    {
+                        charsToRem++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                w = w.Substring(0, w.Length - charsToRem);
 
                 if (tfs.ContainsKey(w))
                 {
