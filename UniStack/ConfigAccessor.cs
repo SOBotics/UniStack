7e8ccad5d438d5f36512f0ebb0961c61acd123f7
Sam <ArcticEcho@users.noreply.github.com>
2018-04-11 17:50:55 +0100

    add configaccessor

---
+++ UniStack/ConfigAccessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace UniStack
+{
+	public static class ConfigAccessor
+	{
+		public static string ConfigFile { get; set; } = "config.json";
+
+		public static T GetValue<T>(string path)
+		{
+			var token = GetToken(path);
+
+			if (token == null)
+			{
+				return default(T);
+			}
+
+			return token.Value<T>();
+		}
+
+
+
+		private static JToken GetToken(string path)
+		{
+			var json = File.ReadAllText(ConfigFile);
+			var obj = JObject.Parse(json);
+
+			return obj.SelectToken(path);
+		}
+	}
+}
--- UniStack/Program.cs
+++ UniStack/Program.cs
@@ -2,11 +2,11 @@
 
 namespace UniStack
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+
 		}
 	}
 }
