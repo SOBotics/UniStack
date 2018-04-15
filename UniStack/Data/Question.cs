333b87f1d4c4a49de0d0deaa1a0d71b98038b3c7
Sam <ArcticEcho@users.noreply.github.com>
2018-04-15 23:00:57 +0100

    Add super-mega-hyper-optimised classes for storing questions in memory

---
+++ UniStack/Data/Question.cs
@@ -0,0 +1,77 @@
+﻿using System.Runtime.InteropServices;
+
+#region question data structure
+
+// int id
+
+// byte tagCount
+// int tag1
+// ...
+
+// short termCount
+// int term1Val
+// byte term1Freq
+// ...
+
+#endregion
+
+namespace UniStack.Data
+{
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public unsafe struct Question
+	{
+		private byte* data;
+
+		public int Id => GetInt(0);
+
+		public byte TagCount => data[4];
+
+		public int GetTag(int index) => GetInt(5 + (index * 4));
+
+		public short TermCount =>
+			(short)(
+				(data[
+					4 +            // Id
+					1 +            // Tag count
+					(4 * TagCount) // Tags
+				] << 8) ^
+				data[
+					4 +              // Id
+					1 +              // Tag count
+					(4 * TagCount) + // Tags
+					1                // Offset
+				]
+			);
+
+		public int GetTermValue(int index) => 
+			GetInt(
+				4 +              // Id
+				1 +              // Tag count
+				(TagCount * 4) + // Tags
+				2 +              // Term count
+				(index * 5)      // Terms
+			);
+
+		public byte GetTermFrequency(int index) =>
+			data[
+				4 +              // Id
+				1 +              // Tag count
+				(TagCount * 4) + // Tags
+				2 +              // Term count
+				(index * 5) +    // Terms
+				4                //Term value
+			];
+
+
+
+		internal Question(byte* data) => this.data = data;
+
+
+
+		private int GetInt(int offset) =>
+			data[offset]     << 24 ^
+			data[offset + 1] << 16 ^
+			data[offset + 2] <<  8 ^
+			data[offset + 3];
+	}
+}
---
+++ UniStack/Data/QuestionPool.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+#region question data structure
+
+// int id
+
+// byte tagCount
+// int tag1
+// ...
+
+// short termCount
+// int term1Val
+// byte term1Freq
+// ...
+
+#endregion
+
+namespace UniStack.Data
+{
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public unsafe struct QuestionPool
+	{
+		private byte* data;
+		private long dataBufferSize;
+		private long position;
+		private int bufferResizeMultiplier;
+
+		public long CurrentSizeBytes;
+
+		public QuestionPool(int bufferResizeMultiplier)
+		{
+			data = default(byte*);
+			dataBufferSize = 0;
+			position = 0;
+			this.bufferResizeMultiplier = bufferResizeMultiplier;
+			CurrentSizeBytes = 0;
+		}
+
+		public void Add(int id, int[] tags, Dictionary<int, byte> terms)
+		{
+			// id + tagCount + tags + termCount + terms
+			var bytes = 4 + 1 + (tags.Length * 4) + 2 + (terms.Count * 5);
+
+			if (CurrentSizeBytes + bytes > dataBufferSize)
+			{
+				var expandBy = bytes * Math.Max(bufferResizeMultiplier, 1);
+
+				dataBufferSize = CurrentSizeBytes + expandBy;
+
+				if (data == default(byte*))
+				{
+					data = (byte*)Marshal.AllocHGlobal(expandBy);
+				}
+				else
+				{
+					data = (byte*)Marshal.ReAllocHGlobal((IntPtr)data, (IntPtr)dataBufferSize);
+				}
+			}
+
+			var offset = CurrentSizeBytes;
+
+			SetInt(id, ref offset);
+			data[offset++] = (byte)tags.Length;
+
+			for (var i = 0; i < tags.Length; i++)
+			{
+				SetInt(tags[i], ref offset);
+			}
+
+			data[offset++] = (byte)(terms.Count >> 8);
+			data[offset++] = (byte)terms.Count;
+
+			foreach (var kv in terms)
+			{
+				SetInt(kv.Key, ref offset);
+				data[offset++] = kv.Value;
+			}
+
+			CurrentSizeBytes += bytes;
+		}
+
+		public void Remove(uint[] indices)
+		{
+			//TODO: Implement.
+			throw new NotImplementedException();
+		}
+
+		public bool Next(out Question question, bool fromStart = false)
+		{
+			if (fromStart)
+			{
+				position = 0;
+			}
+
+			if (position >= CurrentSizeBytes)
+			{
+				question = default(Question);
+
+				return false;
+			}
+
+			var q = new Question(data + position);
+
+			// id
+			position += 4;
+			// tag count
+			position += 1;
+			// tags
+			position += 4 * data[position - 1];
+			// term count
+			position += 2;
+			// terms
+			position += 5 * (data[position - 2] << 8 ^ data[position - 1]);
+
+			question = q;
+
+			return true;
+		}
+
+		public void Destroy() => Marshal.FreeHGlobal((IntPtr)data);
+
+
+
+		private void SetInt(int val, ref long offset)
+		{
+			data[offset++] = (byte)(val >> 24);
+			data[offset++] = (byte)(val >> 16);
+			data[offset++] = (byte)(val >> 8);
+			data[offset++] = (byte)val;
+		}
+	}
+}
